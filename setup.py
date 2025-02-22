554650a79c6c02738356aa15dade2eea7e5aa50a
KiralyBalintZoltan <kiralybalintzoltan@gmail.com>
2025-02-22 16:55:43 +0100

    Rename project settings

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 from glob import glob
 import os
 
-package_name = 'ros2_py_template'
+package_name = 'Kir_I4V_random'
 
 setup(
     name=package_name,
@@ -16,14 +16,14 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='todo',
-    maintainer_email='todo@todo.com',
-    description='TODO: Package description',
+    maintainer='KiralyBalintZoltan',
+    maintainer_email='kiralybalintzoltan@gmail.com',
+    description='Ez egy bevezető csomag',
     license='GNU General Public License v3.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            # 'control_vehicle = ros2_py_template.control_vehicle:main',
+            # 'control_vehicle = Kir_I4V_random.control_vehicle:main',
         ],
     },
 )
