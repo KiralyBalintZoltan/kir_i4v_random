55bd8e9ce73af1e7bc6f35062eed48d7960394d4
KiralyBalintZoltan <kiralybalintzoltan@gmail.com>
2025-03-16 15:02:15 +0100

    launch_example1 Fix

--- launch/launch_example1.launch.py
+++ launch/launch_example1.launch.py
@@ -3,30 +3,19 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
+        # Indítjuk a turtlesim node-ot
         Node(
-            package='turtlesim',
-            namespace='turtlesim1',
-            executable='turtlesim_node',
-            name='sim'
+            package='turtlesim',  
+            executable='turtlesim_node',  
+            name='turtlesim',  
+            output='screen'  
         ),
+        
+        # Indítjuk a project_node node-ot
         Node(
-            package='turtlesim',
-            namespace='turtlesim2',
-            executable='turtlesim_node',
-            name='sim'
-        ),
-        Node(
-            package='turtlesim',
-            executable='mimic',
-            name='mimic',
-            remappings=[
-                ('/input/pose', '/turtlesim1/turtle1/pose'),
-                ('/output/cmd_vel', '/turtlesim2/turtle1/cmd_vel'),
-            ]
-        ),
-         Node(
-             package='ros2_python_template',
-             executable='simple_sub_node',
-             output='screen',
-         ),
+            package='kir_i4v_random',  #
+            executable='draw_random',  
+            name='draw_random1',  
+            output='screen'  
+        )
     ])
